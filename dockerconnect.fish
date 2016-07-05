8fef23792716c7eeb2fc05c4fa362cd90e58ee9d
Cakemix <mholoch@gmail.com>
2016-07-05 11:56:45 +0200

    changed the way dockerconnect works when the container already runs.

--- dockerconnect.fish
+++ dockerconnect.fish
@@ -3,14 +3,15 @@
 # or start -a to connect to the container.
 function dockerconnect
   # Check if the container with name $argv exists/runs
-  set running (docker inspect --format="{{ .State.Running }}" $argv)
+  set running (docker inspect --format="{{ .State.Running }}" $argv[1])
 
   if [ $running = true ]
-    echo "Container already running. exec-ing a new /bin/bash in it."
+    echo "Container $argv[1] already running. Exec-ing a new bash."
     docker exec -it $argv[1] /ros_entrypoint.sh /bin/bash
   else
-    echo "Container is not running. Starting and attaching to it."
-    docker start -a $argv[1]
+    echo "Container $argv[1] is not running. Starting and attaching to it."
+    docker start $argv[1]
+    docker attach $argv[1]
   end
 
 end
